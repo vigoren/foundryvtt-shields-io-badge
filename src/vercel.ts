9832802d27f8d6a7ae636108f1c6aadddab1cd59
Dean Vigoren <vigorator@gmail.com>
2025-03-20 16:10:04 -0600

    Testing vercel

--- src/vercel.ts
+++ src/vercel.ts
@@ -1,14 +1,18 @@
 "use strict";
 import Fastify, {FastifyReply, FastifyRequest} from "fastify";
-import Server from "./classes/server.js";
+//import Server from "./classes/server.js";
 
-const server = new Server(process.env.PORT || "3000");
+//const server = new Server(process.env.PORT || "3000");
 const app = Fastify({
     logger: true,
 });
 
+app.get('/', async (req, reply) => {
+    return reply.status(200).type('text/html').send('<html><body><h1>Welcome to the Vercel server!</h1></body></html>');
+})
+
 // Register your application as a normal plugin.
-app.register(server.routes);
+//app.register(server.routes);
 
 export default async function handler (req:FastifyRequest, res: FastifyReply) {
     await app.ready();
